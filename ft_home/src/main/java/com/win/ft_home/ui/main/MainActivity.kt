378f2795878dc9ea2dbda348923932856385f223
李文 <liwen@ppdai.com>
2020-06-03 11:12:05 +0800

    处理首页返回键逻辑

--- ft_home/src/main/java/com/win/ft_home/ui/main/MainActivity.kt
+++ ft_home/src/main/java/com/win/ft_home/ui/main/MainActivity.kt
@@ -16,8 +16,10 @@ import com.win.lib_base.base.BaseActivity
 import com.win.lib_base.base.FixFragmentNavigator
 import com.win.lib_base.service.login.warp.LoginServiceImplWrap
 
-class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>()
-{
+class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
+
+    private lateinit var navView: BottomNavigationView
+    private lateinit var navController: NavController
 
     override fun getLayoutResId(): Int = R.layout.activity_main
 
@@ -31,10 +33,10 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>()
 //        navView.setupWithNavController(navController)
 
 
-        val navView: BottomNavigationView = mViewBinding.navView
+        navView = mViewBinding.navView
 
         //添加自定义的FixFragmentNavigator
-        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
         val fragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val fragmentNavigator =
@@ -52,6 +54,21 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>()
     }
 
     override fun initView() {
+
+    }
+
+    override fun onBackPressed() {
+
+//        如果调用 super.onBackPressed()，navigation会操作回退栈,切换到之前显示的页面，导致 页面叠加错乱
+//        super.onBackPressed()
+
+        val id = navController.currentDestination?.id!!
+        val homeNavi = navController.graph[R.id.navigation_home].id
+        if (id != homeNavi) {
+            navView.selectedItemId = R.id.navigation_home
+        } else {
+            finish()
+        }
 
     }
 
